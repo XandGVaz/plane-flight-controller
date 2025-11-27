2ce0ba8043fd741ec7f2bc11aed58ad24d4777d3
XandGVaz <vitorgarciavaz@gmail.com>
2025-11-27 18:54:57 -0300

    feat: criação objetos dos servos

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,59 @@
+
+/*===============================================================================*/
+// Inclusão de bibliotecas necessárias
+
+// Biblioteca Arduino
 #include <Arduino.h>
+
+// Biblioteca do servo
+#include "servo.hpp"
+
+// Biblioteca para display
+#include "TFT_eSPI.h"
+
+/*===============================================================================*/
+// Pinagem dos componentes
+
+// Pinos servo flaps 
+#define FLAP_LEFT_SERVO_PIN 2
+#define FLAP_LEFT_SERVO_CHANNEL 0
+#define FLAP_RIGHT_SERVO_PIN 3
+#define FLAP_RIGHT_SERVO_CHANNEL 1
+
+// Pinos servo ailerons
+#define AILERON_LEFT_SERVO_PIN 4
+#define AILERON_LEFT_SERVO_CHANNEL 2
+#define AILERON_RIGHT_SERVO_PIN 5
+#define AILERON_RIGHT_SERVO_CHANNEL 3
+
+// Pinos servo profundor
+#define ELEVATOR_LEFT_SERVO_PIN 6
+#define ELEVATOR_LEFT_SERVO_CHANNEL 4
+#define ELEVATOR_RIGHT_SERVO_PIN 7
+#define ELEVATOR_RIGHT_SERVO_CHANNEL 5
+
+// Pino servo leme
+#define RUDDER_SERVO_PIN 8
+#define RUDDER_SERVO_CHANNEL 6
+
+/*===============================================================================*/
+// Variáveis globais
+
+// Servos flaps
+Servo flapLeftServo(FLAP_LEFT_SERVO_PIN, FLAP_LEFT_SERVO_CHANNEL);
+Servo flapRightServo(FLAP_RIGHT_SERVO_PIN, FLAP_RIGHT_SERVO_CHANNEL);
+
+// Servos ailerons
+Servo aileronLeftServo(AILERON_LEFT_SERVO_PIN, AILERON_LEFT_SERVO_CHANNEL);
+Servo aileronRightServo(AILERON_RIGHT_SERVO_PIN, AILERON_RIGHT_SERVO_CHANNEL);
+
+// Servos profundor
+Servo elevatorLeftServo(ELEVATOR_LEFT_SERVO_PIN, ELEVATOR_LEFT_SERVO_CHANNEL);
+Servo elevatorRightServo(ELEVATOR_RIGHT_SERVO_PIN, ELEVATOR_RIGHT_SERVO_CHANNEL);
+
+// Servo leme
+Servo rudderServo(RUDDER_SERVO_PIN, RUDDER_SERVO_CHANNEL);
+
 
 void setup() {
   // put your setup code here, to run once:
