dd6973f605da490324478990ad9910bbdefe9a23
XandGVaz <vitorgarciavaz@gmail.com>
2025-12-08 17:27:44 -0300

    feat: inclusão de tasks RTOS

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -83,6 +83,33 @@ Servo ElevatorRightServo(ELEVATOR_RIGHT_SERVO_PIN, ELEVATOR_RIGHT_SERVO_CHANNEL)
 Servo RudderServo(RUDDER_SERVO_PIN, RUDDER_SERVO_CHANNEL);
 
 /*===============================================================================*/
+// Variáveis FreeRTOS
+
+// Tasks FreeRTOS
+xTaskHandle xDisplayWriteTaskHandle = NULL;
+xTaskHandle xSDCardSaveTaskHandle = NULL;
+xTaskHandle xJoystickReadTaskHandle = NULL;
+xTaskHandle xControlTaskHandle = NULL;
+
+// Prototypes de funções das tasks
+void vDisplayWriteTask(void *pvParameters);
+void vSDCardSaveTask(void *pvParameters);
+void vJoystickReadTask(void *pvParameters);
+void vControlTask(void *pvParameters);
+
+// Prototype de callback de timer de leitura do joystick
+void vJoystickReadTimerCallback(TimerHandle_t xTimer);
+
+// Queues FreeRTOS
+xQueueHandle xFlightStateToDisplay = NULL;
+xQueueHandle xFlightStateToSDCard = NULL;
+xQueueHandle xFlightStateToControl = NULL;
+
+// Timers FreeRTOS
+xTimerHandle xJoystickReadTimerHandle = NULL;
+
+
+/*===============================================================================*/
 // Estados de voo
 
 // Enum de possíveis estados de voo
@@ -104,9 +131,221 @@ String flightStatesTexts[] = {
 /*===============================================================================*/
 
 void setup() {
-  // put your setup code here, to run once:
+  // Inicialização da serial para debug
+  Serial.begin(115200);
+
+  // Inicialização do display
+  if(!Display.setup()){
+    Serial.println("Erro na inicialização do display I2C!");
+    while(1);
+  } 
+
+  // Inicialização dos servos
+  if(!FlapLeftServo.setup()){
+    Serial.println("Erro na inicialização do servo Flap Esquerdo!");
+    while(1);
+  }
+  if(!FlapRightServo.setup()){
+    Serial.println("Erro na inicialização do servo Flap Direito!");
+    while(1);
+  }
+  if(!AileronLeftServo.setup()){
+    Serial.println("Erro na inicialização do servo Aileron Esquerdo!");
+    while(1);
+  }
+  if(!AileronRightServo.setup()){
+    Serial.println("Erro na inicialização do servo Aileron Direito!");
+    while(1);
+  }
+  if(!ElevatorLeftServo.setup()){
+    Serial.println("Erro na inicialização do servo Profundor Esquerdo!");
+    while(1);
+  }
+  if(!ElevatorRightServo.setup()){
+    Serial.println("Erro na inicialização do servo Profundor Direito!");
+    while(1);
+  }
+  if(!RudderServo.setup()){
+    Serial.println("Erro na inicialização do servo Leme!");
+    while(1);
+  }
+
+  // Inicialização do cartão SD
+  SDCardLogger sdLogger(SD_CS);
+  if(!sdLogger.begin()){
+    Serial.println("Erro na inicialização do cartão SD!");
+    while(1);
+  } 
+
+  // Criação das filas
+  xFlightStateToDisplay = xQueueCreate(5, sizeof(flightState));
+  xFlightStateToSDCard = xQueueCreate(5, sizeof(flightState));
+  xFlightStateToControl = xQueueCreate(5, sizeof(flightState));
+
+  // Criação dos software timers
+  xJoystickReadTimerHandle = xTimerCreate("JOYSTICK_READ_TIMER", pdMS_TO_TICKS(100), pdTRUE, NULL, vJoystickReadTimerCallback);
+  if(xJoystickReadTaskHandle == NULL){
+    Serial.println("Erro na criação do timer de leitura do joystick!");
+    while(1);
+  }
+
+  // Criação das tasks
+  xTaskCreatePinnedToCore(vDisplayWriteTask, "DISPLAY_WRITE_TASK", 4096, NULL, 1, &xDisplayWriteTaskHandle, APP_CPU_NUM);
+  if(xDisplayWriteTaskHandle == NULL){
+    Serial.println("Erro na criação da task de escrita no display!");
+    while(1);
+  }
+  xTaskCreatePinnedToCore(vSDCardSaveTask, "SDCARD_SAVE_TASK", 8192, NULL, 1, &xSDCardSaveTaskHandle, APP_CPU_NUM);
+  if(xSDCardSaveTaskHandle == NULL){
+    Serial.println("Erro na criação da task de salvamento no cartão SD!");
+    while(1);
+  }
+  xTaskCreatePinnedToCore(vJoystickReadTask, "JOYSTICK_READ_TASK", 4096, NULL, 1, &xJoystickReadTaskHandle, PRO_CPU_NUM);
+  if(xJoystickReadTaskHandle == NULL){
+    Serial.println("Erro na criação da task de leitura do joystick!");
+    while(1);
+  }
+  xTaskCreatePinnedToCore(vControlTask, "CONTROL_TASK", 8192, NULL, 1, &xControlTaskHandle, PRO_CPU_NUM);
+  if(xControlTaskHandle == NULL){
+    Serial.println("Erro na criação da task de controle dos servos!");
+    while(1);
+  }
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  vTaskDelete(NULL);
+}
+
+/*===============================================================================*/
+// Definição da callback do timer de leitura do joystick
+
+void vJoystickReadTimerCallback(TimerHandle_t xTimer){
+  // Notificação para a task de leitura do joystick
+  vTaskNotifyGiveFromISR(xJoystickReadTaskHandle, NULL);
+}
+
+/*===============================================================================*/
+// Definição das tasks FreeRTOS
+
+// Task de escrita no display
+void vDisplayWriteTask(void *pvParameters){
+  // Variável para receber o estado de voo da fila
+  flightState receivedFlightState;
+  
+  while(1){
+    // Espera por um estado de voo na fila
+    if(xQueueReceive(xFlightStateToDisplay, &receivedFlightState, portMAX_DELAY) == pdTRUE){
+      // Escreve o estado de voo no display
+      Display.clear();
+      Display.writeMessage("Estado Voo:", 0);
+      Display.writeMessage(flightStatesTexts[receivedFlightState],1);
+    }
+  }
+}
+
+// Task de salvamento no cartão SD
+void vSDCardSaveTask(void *pvParameters){
+  // Variável para receber o estado de voo da fila
+  flightState receivedFlightState;
+
+  // Instanciação do logger do cartão SD
+  SDCardLogger sdLogger(SD_CS);
+
+  while(1){
+    // Espera por um estado de voo na fila
+    if(xQueueReceive(xFlightStateToSDCard, &receivedFlightState, portMAX_DELAY) == pdTRUE){
+      // Formata a linha de dados para salvar
+      String dataLine = String(millis()) + "," + flightStatesTexts[receivedFlightState];
+      
+      // Salva a linha no cartão SD
+      sdLogger.writeLine(dataLine);
+    }
+  }
+}
+
+// Task de leitura do joystick
+void vJoystickReadTask(void *pvParameters){
+  // Inicialização do timer de leitura do joystick
+  xTimerStart(xJoystickReadTimerHandle, 0);
+
+  while(1){
+    // Espera pela notificação do timer
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+    // Leitura dos valores do joystick
+    int16_t xValue = analogRead(JOYSTICK_ADC_X_PIN);
+    int16_t yValue = analogRead(JOYSTICK_ADC_y_PIN);
+
+    // Determinação do estado de voo baseado nos valores lidos
+    flightState currentFlightState;
+    if(yValue > 3000){
+      currentFlightState = PITCH_UP;
+    } else if(yValue < 1000){
+      currentFlightState = PITCH_DOWN;
+    } else if(xValue > 3000){
+      currentFlightState = ROLL_RIGHT;
+    } else if(xValue < 1000){
+      currentFlightState = ROLL_LEFT;
+    } else {
+      continue; // Nenhuma ação detectada
+    }
+
+    // Envio do estado de voo para as filas correspondentes
+    xQueueSend(xFlightStateToDisplay, &currentFlightState, 0);
+    xQueueSend(xFlightStateToSDCard, &currentFlightState, 0);
+    xQueueSend(xFlightStateToControl, &currentFlightState, 0);
+  }
+}
+
+
+// Task de controle dos servos
+void vControlTask(void *pvParameters){
+  // Variável para receber o estado de voo da fila
+  flightState receivedFlightState; 
+
+  // Seta todos os servos na posição neutra
+  FlapLeftServo.setAngle(90);
+  FlapRightServo.setAngle(90);
+  AileronLeftServo.setAngle(90);
+  AileronRightServo.setAngle(90);
+  ElevatorLeftServo.setAngle(90);
+  ElevatorRightServo.setAngle(90);
+  RudderServo.setAngle(90);
+
+  while(1){
+    // Espera por um estado de voo na fila
+    if(xQueueReceive(xFlightStateToControl, &receivedFlightState, portMAX_DELAY) == pdTRUE){
+      
+      // Controle dos servos baseado no estado de voo recebido
+      switch(receivedFlightState){
+        // Controle para subida
+        case PITCH_UP:
+          ElevatorLeftServo.setAngle(30);
+          ElevatorRightServo.setAngle(30);
+          break;
+        
+        // Controle para descida
+        case PITCH_DOWN:
+          ElevatorLeftServo.setAngle(150);
+          ElevatorRightServo.setAngle(150);
+          break;
+        
+        // Controle para virar à direita
+        case ROLL_RIGHT:
+          AileronLeftServo.setAngle(150);
+          AileronRightServo.setAngle(30);
+          break;
+        
+        // Controle para virar à esquerda
+        case ROLL_LEFT:
+          AileronLeftServo.setAngle(30);
+          AileronRightServo.setAngle(150);
+          break;
+        
+        default:
+          break;
+      }
+    }
+  }
 }
